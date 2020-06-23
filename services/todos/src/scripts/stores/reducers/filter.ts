0dc015948c408e921bb80991034a4601d1aeac6b
Guo Zile <zlguo1996@gmail.com>
2020-06-23 18:45:42 +0900

    fix bug in filter store

--- services/todos/src/scripts/stores/reducers/filter.ts
+++ services/todos/src/scripts/stores/reducers/filter.ts
@@ -1,12 +1,12 @@
 import {FilterState, FilterActionTypes} from '../actions/filterActions'
 import {MODIFY_FILTER} from '../actions/types'
 
-const initialState = {
+const initialState: FilterState = {
     visible: ['Completed', 'Incomplete']
 }
 
 export default function FilterReducer(
-    state: FilterState,
+    state: FilterState = initialState,
     action: FilterActionTypes,
 ): FilterState {
     switch (action.type) {
