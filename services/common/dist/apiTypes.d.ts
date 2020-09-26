3c9bc04694f64b20a46a23863da2db94d16c5f14
Guo Zile <zlguo1996@gmail.com>
2020-09-26 18:01:25 +0900

    remove track on dist folder

--- services/common/dist/apiTypes.d.ts
+++
@@ -1,3 +0,0 @@
-import { FullTodoItem } from './itemTypes';
-export declare type ModifyItem = Partial<FullTodoItem> & Pick<FullTodoItem, 'id'>;
-export declare type AddItem = Omit<FullTodoItem, 'id' | 'order'>;
